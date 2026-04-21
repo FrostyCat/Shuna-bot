921aadc9c6cf03b60adbd32b9f68c0b0daa7c8e2
Frosty <bill.letu10@gmail.com>
2026-04-21 23:14:26 +0200

    dodanie naprawy

---
+++ remove_duplicates.py
@@ -0,0 +1,28 @@
+from db import Session, init_db
+from models import Attack
+from sqlalchemy import func
+
+init_db()
+session = Session()
+
+all_attacks = session.query(Attack).order_by(Attack.id).all()
+
+seen = {}
+to_delete = []
+
+for attack in all_attacks:
+    key = (attack.player_id, attack.defender, attack.stars, attack.destruction, attack.is_attack, attack.created_at)
+    if key in seen:
+        to_delete.append(attack)
+    else:
+        seen[key] = attack.id
+
+print(f"Found {len(to_delete)} duplicates to remove.")
+
+for attack in to_delete:
+    session.delete(attack)
+
+session.commit()
+session.close()
+
+print("Done.")
