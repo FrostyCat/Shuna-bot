25aca5449b16e43b30ff3d395e02462426cde4db
Frosty <bill.letu10@gmail.com>
2026-04-23 15:11:38 +0200

    FIxing URL problems

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
