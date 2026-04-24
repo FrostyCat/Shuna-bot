4c3abf596ccea6a50848b55234504d215215fe22
Frosty <bill.letu10@gmail.com>
2026-04-25 01:52:24 +0200

    Checking guilds

--- bot.py
+++ bot.py
@@ -29,6 +29,8 @@ COGS = [
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    for guild in bot.guilds:
+        print(f"  - {guild.name} ({guild.id})")
 
 
 @bot.command()
